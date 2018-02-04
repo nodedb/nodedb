dc46268956ca024393863ec9ee613113ca11f85d
Simon Emms <simon@simonemms.com>
2018-02-04 17:07:23 +0000

    Fixed linting issues in .electron-vue folder

--- .electron-vue/webpack.main.config.js
+++ .electron-vue/webpack.main.config.js
@@ -1,19 +1,19 @@
-'use strict'
 
-process.env.BABEL_ENV = 'main'
 
-const path = require('path')
-const { dependencies } = require('../package.json')
-const webpack = require('webpack')
+process.env.BABEL_ENV = 'main';
 
-const BabiliWebpackPlugin = require('babili-webpack-plugin')
+const path = require('path');
+const { dependencies } = require('../package.json');
+const webpack = require('webpack');
 
-let mainConfig = {
+const BabiliWebpackPlugin = require('babili-webpack-plugin');
+
+const mainConfig = {
   entry: {
-    main: path.join(__dirname, '../src/main/index.js')
+    main: path.join(__dirname, '../src/main/index.js'),
   },
   externals: [
-    ...Object.keys(dependencies || {})
+    ...Object.keys(dependencies || {}),
   ],
   module: {
     rules: [
@@ -23,46 +23,44 @@ let mainConfig = {
         exclude: /node_modules/,
         use: {
           loader: 'eslint-loader',
-        }
+        },
       },
       {
         test: /\.js$/,
         use: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.node$/,
-        use: 'node-loader'
-      }
-    ]
+        use: 'node-loader',
+      },
+    ],
   },
   node: {
     __dirname: process.env.NODE_ENV !== 'production',
-    __filename: process.env.NODE_ENV !== 'production'
+    __filename: process.env.NODE_ENV !== 'production',
   },
   output: {
     filename: '[name].js',
     libraryTarget: 'commonjs2',
-    path: path.join(__dirname, '../dist/electron')
+    path: path.join(__dirname, '../dist/electron'),
   },
   plugins: [
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
   resolve: {
-    extensions: ['.js', '.json', '.node']
+    extensions: ['.js', '.json', '.node'],
   },
-  target: 'electron-main'
-}
+  target: 'electron-main',
+};
 
 /**
  * Adjust mainConfig for development settings
  */
 if (process.env.NODE_ENV !== 'production') {
-  mainConfig.plugins.push(
-    new webpack.DefinePlugin({
-      '__static': `"${path.join(__dirname, '../static').replace(/\\/g, '\\\\')}"`
-    })
-  )
+  mainConfig.plugins.push(new webpack.DefinePlugin({
+    __static: `"${path.join(__dirname, '../static').replace(/\\/g, '\\\\')}"`,
+  }));
 }
 
 /**
@@ -72,9 +70,9 @@ if (process.env.NODE_ENV === 'production') {
   mainConfig.plugins.push(
     new BabiliWebpackPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    })
-  )
+      'process.env.NODE_ENV': '"production"',
+    }),
+  );
 }
 
-module.exports = mainConfig
+module.exports = mainConfig;
--- .electron-vue/webpack.web.config.js
+++ .electron-vue/webpack.web.config.js
@@ -1,19 +1,19 @@
-'use strict'
 
-process.env.BABEL_ENV = 'web'
 
-const path = require('path')
-const webpack = require('webpack')
+process.env.BABEL_ENV = 'web';
 
-const BabiliWebpackPlugin = require('babili-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
 
-let webConfig = {
+const BabiliWebpackPlugin = require('babili-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const webConfig = {
   devtool: '#cheap-module-eval-source-map',
   entry: {
-    web: path.join(__dirname, '../src/renderer/main.js')
+    web: path.join(__dirname, '../src/renderer/main.js'),
   },
   module: {
     rules: [
@@ -23,24 +23,24 @@ let webConfig = {
         exclude: /node_modules/,
         use: {
           loader: 'eslint-loader',
-        }
+        },
       },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: 'css-loader'
-        })
+          use: 'css-loader',
+        }),
       },
       {
         test: /\.html$/,
-        use: 'vue-html-loader'
+        use: 'vue-html-loader',
       },
       {
         test: /\.js$/,
         use: 'babel-loader',
-        include: [ path.resolve(__dirname, '../src/renderer') ],
-        exclude: /node_modules/
+        include: [path.resolve(__dirname, '../src/renderer')],
+        exclude: /node_modules/,
       },
       {
         test: /\.vue$/,
@@ -50,10 +50,10 @@ let webConfig = {
             extractCSS: true,
             loaders: {
               sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax=1',
-              scss: 'vue-style-loader!css-loader!sass-loader'
-            }
-          }
-        }
+              scss: 'vue-style-loader!css-loader!sass-loader',
+            },
+          },
+        },
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -61,9 +61,9 @@ let webConfig = {
           loader: 'url-loader',
           query: {
             limit: 10000,
-            name: 'imgs/[name].[ext]'
-          }
-        }
+            name: 'imgs/[name].[ext]',
+          },
+        },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
@@ -71,11 +71,11 @@ let webConfig = {
           loader: 'url-loader',
           query: {
             limit: 10000,
-            name: 'fonts/[name].[ext]'
-          }
-        }
-      }
-    ]
+            name: 'fonts/[name].[ext]',
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new ExtractTextPlugin('styles.css'),
@@ -85,35 +85,35 @@ let webConfig = {
       minify: {
         collapseWhitespace: true,
         removeAttributeQuotes: true,
-        removeComments: true
+        removeComments: true,
       },
-      nodeModules: false
+      nodeModules: false,
     }),
     new webpack.DefinePlugin({
-      'process.env.IS_WEB': 'true'
+      'process.env.IS_WEB': 'true',
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
   output: {
     filename: '[name].js',
-    path: path.join(__dirname, '../dist/web')
+    path: path.join(__dirname, '../dist/web'),
   },
   resolve: {
     alias: {
       '@': path.join(__dirname, '../src/renderer'),
-      'vue$': 'vue/dist/vue.esm.js'
+      vue$: 'vue/dist/vue.esm.js',
     },
-    extensions: ['.js', '.vue', '.json', '.css']
+    extensions: ['.js', '.vue', '.json', '.css'],
   },
-  target: 'web'
-}
+  target: 'web',
+};
 
 /**
  * Adjust webConfig for production settings
  */
 if (process.env.NODE_ENV === 'production') {
-  webConfig.devtool = ''
+  webConfig.devtool = '';
 
   webConfig.plugins.push(
     new BabiliWebpackPlugin(),
@@ -121,16 +121,16 @@ if (process.env.NODE_ENV === 'production') {
       {
         from: path.join(__dirname, '../static'),
         to: path.join(__dirname, '../dist/web/static'),
-        ignore: ['.*']
-      }
+        ignore: ['.*'],
+      },
     ]),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
+      'process.env.NODE_ENV': '"production"',
     }),
     new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  )
+      minimize: true,
+    }),
+  );
 }
 
-module.exports = webConfig
+module.exports = webConfig;
