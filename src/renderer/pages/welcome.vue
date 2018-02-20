8dc89ee73e1bbb2a9e95d1fc810aca5ede235ed0
Simon Emms <simon@simonemms.com>
2018-02-20 22:01:46 +0000

    Stuff

---
+++ src/renderer/pages/welcome.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  div welcome page
+</template>
+
+<script>
+  /**
+   * welcome
+   */
+
+  /* Node modules */
+
+  /* Third-party modules */
+
+  /* Files */
+
+  export default {
+    name: 'welcome',
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
