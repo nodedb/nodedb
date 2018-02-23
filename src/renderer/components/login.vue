762b2659947c4df02441cf67aa14d31c07139b71
Simon Emms <simon@simonemms.com>
2018-02-23 22:07:14 +0000

    Start of the login page

---
+++ src/renderer/components/login.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  div
+    el-form-item(
+      label="twat"
+    )
+</template>
+
+<script>
+  /**
+   * login
+   */
+
+  /* Node modules */
+
+  /* Third-party modules */
+
+  /* Files */
+
+  export default {
+    name: 'login-form',
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/renderer/pages/login.vue
+++ src/renderer/pages/login.vue
@@ -1,5 +1,42 @@
 <template lang="pug">
-  div new connection
+  div
+    div {{ form }}
+    el-alert(
+      v-if="drivers.length === 0"
+      :title="$t('drivers:NO_DRIVERS_INSTALLED_TITLE')"
+      type="warning"
+      show-icon
+      :closable="false"
+    )
+      p.el-alert__description {{ $t('drivers:NO_DRIVERS_INSTALLED_DESC') }}
+
+    el-form(
+      v-else
+      ref="form"
+      :model="form"
+      :label-width="labelWidth + 'px'"
+    )
+
+      el-form-item( :label="$t('drivers:SELECT_DRIVER')" )
+        el-select(
+          v-model="form.driver"
+          :placeholder="$t('drivers:SELECT_DRIVER')"
+        )
+          el-option(
+            v-for="item in drivers"
+            :label="item.name"
+            :value="item.id"
+          )
+
+      login-form
+
+      el-form-item
+        el-button(
+          type="primary"
+          @click="onSubmit"
+        ) @todo
+
+        el-button Cancel
 </template>
 
 <script>
@@ -12,12 +49,56 @@
   /* Third-party modules */
 
   /* Files */
+  import loginForm from '../components/login';
 
   export default {
     name: 'login',
+
+    components: {
+      loginForm,
+    },
+
+    computed: {
+      drivers: vm => vm.$store.state.drivers.drivers,
+      tabId: vm => vm.$route.params.tabId,
+    },
+
+    created() {
+      console.log(this.tabId);
+    },
+
+    data() {
+      return {
+        active: null,
+        form: {
+          connection: {},
+          driver: null,
+        },
+        labelWidth: 120,
+      };
+    },
+
+    methods: {
+      onSubmit() {
+        console.log('submit');
+        console.log(this.form);
+      },
+    },
+
+    watch: {
+      tabId(id) {
+        console.log({
+          id,
+        });
+      },
+    },
   };
 </script>
 
 <style lang="scss" scoped>
+
+  .el-select, .el-input {
+    width: 100%;
+  }
 
 </style>
